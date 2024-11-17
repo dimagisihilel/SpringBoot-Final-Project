eb83dfa34e2867e84a8f9b3cee3133668fc61b3a
dimagisihilel <gunarathneedirimuni@gmail.com>
2024-11-17 19:51:47 +0530

    Add model mapper

--- green-shadow-project-backend/src/main/java/lk/ijse/greenshadowprojectbackend/GreenShadowProjectBackendApplication.java
+++ green-shadow-project-backend/src/main/java/lk/ijse/greenshadowprojectbackend/GreenShadowProjectBackendApplication.java
@@ -1,7 +1,9 @@
 package lk.ijse.greenshadowprojectbackend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GreenShadowProjectBackendApplication {
@@ -10,4 +12,8 @@ public class GreenShadowProjectBackendApplication {
         SpringApplication.run(GreenShadowProjectBackendApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }

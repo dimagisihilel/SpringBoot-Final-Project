bc2389c53e4ec59e6a566ad472ab93e424422467
dimagisihilel <gunarathneedirimuni@gmail.com>
2024-11-17 19:41:24 +0530

    Create backend project

---
+++ green-shadow-project-backend/src/test/java/lk/ijse/greenshadowprojectbackend/GreenShadowProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.greenshadowprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenShadowProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

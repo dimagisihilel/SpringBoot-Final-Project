834f71316855d8b99f9efff92e22cf115cf20806
dimagisihilel <gunarathneedirimuni@gmail.com>
2024-11-23 18:36:06 +0530

    Update log service

--- green-shadow-project-backend/src/main/java/lk/ijse/greenshadowprojectbackend/service/LogService.java
+++ green-shadow-project-backend/src/main/java/lk/ijse/greenshadowprojectbackend/service/LogService.java
@@ -3,5 +3,8 @@ package lk.ijse.greenshadowprojectbackend.service;
 
 import lk.ijse.greenshadowprojectbackend.dto.impl.LogDto;
 
+import java.util.Map;
+
 public interface LogService extends BaseService<LogDto> {
+    Map<String, Object> getRelatedEntitiesAsDtos(String logId);
 }

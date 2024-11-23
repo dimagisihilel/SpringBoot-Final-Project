dc7f57f912d8144b8ba03bb1a17a0aa1b13379ef
dimagisihilel <gunarathneedirimuni@gmail.com>
2024-11-23 18:46:28 +0530

    Update dtos

--- green-shadow-project-backend/src/main/java/lk/ijse/greenshadowprojectbackend/dto/impl/FieldDto.java
+++ green-shadow-project-backend/src/main/java/lk/ijse/greenshadowprojectbackend/dto/impl/FieldDto.java
@@ -18,17 +18,7 @@ public class FieldDto implements FieldStatus {
     private Double size;
     private String image1;
     private String image2;
-    /*private Set<StaffDto> staffMembers; // Many-to-many relationship with Staff
-    private Set<CropDto> crops; // One-to-many relationship with Crop*/
     private Set<String> staffIds; // Many-to-many relationship with Staff
     //private Set<CropDto> crops; // One-to-many relationship with Crop
 
-    /*public FieldDto(String fieldId,String name,String location,Double size,String image1,String image2){
-        this.fieldId = fieldId;
-        this.name =name;
-        this.location=location;
-        this.size=size;
-        this.image1=image1;
-        this.image2=image2;
-    }*/
 }
